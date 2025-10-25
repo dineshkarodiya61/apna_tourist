994f5fde36a90648293d9c7a6b3972ac9aacfaa0
Dinesh karodiya <dineshkarodiya61@gmail.com>
2025-10-25 07:51:41 +0530

    tourist

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.querySelector('.card#about').addEventListener('mouseenter', () => {
   document.querySelector('.card#about').scrollIntoView({ behavior: 'smooth', block: 'start' });
 });
+
 const createBtn = document.getElementById('createAccountBtn');
 const modal = document.getElementById('accountModal');
 const closeModal = document.getElementById('closeModal');
@@ -18,32 +19,15 @@ closeModal.addEventListener('click', () => {
 backBtn.addEventListener('click', () => {
   modal.style.display = 'none';
 });
-
 window.addEventListener('click', (e) => {
   if (e.target === modal) modal.style.display = 'none';
 });
-
 accountForm.addEventListener('submit', (e) => {
   e.preventDefault();
   alert('Account Created Successfully!');
   modal.style.display = 'none';
 });
-
 loginBtn.addEventListener('click', () => {
   alert('Redirecting to Login...');
   modal.style.display = 'none';
 });
-
-
-const hamburger = document.getElementById('hamburger');
-const sidebar = document.getElementById('sidebar');
-const closeBtn = document.getElementById('closeBtn');
-
-hamburger.addEventListener('click', () => {
-  sidebar.style.right = '0'; 
-});
-
-closeBtn.addEventListener('click', () => {
-  sidebar.style.right = '-280px'; 
-});
-
