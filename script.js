09fd9b19387ffbfad7c685b5940dcc0243ec40ac
Dinesh karodiya <dineshkarodiya61@gmail.com>
2025-10-29 10:09:27 +0530

    tourist

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const backBtn = document.getElementById('backBtn');
 const backLoginBtn = document.getElementById('backLogin');
 const forgotBtn = document.getElementById('forgotBtn');
 const backForgotBtn = document.getElementById('backForgot');
-
 const closeAccountModal = document.getElementById('closeAccountModal');
 const closeLoginModal = document.getElementById('closeLoginModal');
 const closeForgotModal = document.getElementById('closeForgotModal');
@@ -17,37 +16,28 @@ const closeForgotModal = document.getElementById('closeForgotModal');
 createAccountBtn.onclick = () => {
   accountModal.style.display = 'flex';
 };
-
-
 loginModalBtn.onclick = () => {
   loginModal.style.display = 'flex';
 };
-
-
 goLoginBtn.onclick = () => {
   accountModal.style.display = 'none';
   loginModal.style.display = 'flex';
 };
 
-
 backBtn.onclick = () => {
   accountModal.style.display = 'none';
 };
-
 backLoginBtn.onclick = () => {
   loginModal.style.display = 'none';
 };
-
 forgotBtn.onclick = () => {
   loginModal.style.display = 'none';
   forgotModal.style.display = 'flex';
 };
-
 backForgotBtn.onclick = () => {
   forgotModal.style.display = 'none';
   loginModal.style.display = 'flex';
 };
-
 closeAccountModal.onclick = () => (accountModal.style.display = 'none');
 closeLoginModal.onclick = () => (loginModal.style.display = 'none');
 closeForgotModal.onclick = () => (forgotModal.style.display = 'none');
@@ -60,7 +50,6 @@ window.onclick = (e) => {
 
 const menuToggle = document.getElementById('menuToggle');
 const navLinks = document.getElementById('nav-links');
-
 menuToggle.onclick = () => {
   navLinks.classList.toggle('active');
 };
